80f23ba4c1e3e156c7a86f771627f68284f3343d
huangcheng <hc9725@gmail.com>
2012-06-01 18:04:27 +0800

    main

--- main.c
+++ main.c
@@ -10,47 +10,9 @@
 #include "app.h"
 #include "argparse.h"
 
-struct cp { char *name; cmdhandler cb; };
-
-void do_rqst(char *s)
-{
-	int n;
-	char *p = s;
-	char *cmd = NULL;
-	const struct cp* cp;
-	dict *d;
-
-	p = str_next(s, &n);
-	if (p == NULL)
-		return;
-
-	cmd = strndupa(p, n);
-	printf("cmd={%s}\n", cmd);
-	p += n;
-
-	d = dict_new();
-
-	parse_kvstring( p, d );
-	if ( (cp=findcp(cmd, n )) != NULL ) {
-		cp->cb( (void *)d );
-	}
-
-	dict_free( d );
-
-	return;
-}
 
 int main()
 {
 
-	char *s;
-
-	scli_init();
-	scli_set_option("prompt", "# ");
-
-	while ((s = scli_getline()) != NULL) {
-		do_rqst(s);
-	}
-
-	exit(0);
+printf("hello world");
 }
