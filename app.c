7cf51aaca090829b81358b1238b24a6d7c646444
huangcheng <hc9725@gmail.com>
2012-06-01 17:58:26 +0800

    bad app

--- app.c
+++ app.c
@@ -1,43 +1,4 @@
-
-#include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include "app.h"
-
-static char *ipaddress(int ip)
+int app()
 {
-	struct in_addr in;
-	in.s_addr=ip;
-	return(inet_ntoa(in));
-}
-
-int add_rule_class_entry(struct ce *ce)
-{
-	printf("add:\n");
-	printf("%s\n",ipaddress(ce->sip));
-	printf("%s\n",ipaddress(ce->dip));
-	printf("%d\n",ce->sport);
-	printf("%d\n",ce->dport);
-	printf("%llu\n",ce->protocol);
 	return 0;
 }
-
-
-int del_rule_class_entry(struct ce *ce)
-{
-	printf("del:\n");
-	printf("%s\n",ipaddress(ce->sip));
-	printf("%s\n",ipaddress(ce->dip));
-	printf("%d\n",ce->sport);
-	printf("%d\n",ce->dport);
-	printf("%llu\n",ce->protocol);
-	return 0;
-}
-
-int show_port_counter(int port)
-{
-	printf("sport=%d\n",port);
-	return 0;
-}
-
